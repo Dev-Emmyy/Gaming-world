4611424292b7a4de29b183f365745e1fb2256967
Gunna9999 <emmanuelugochukwu2000@gmail.com>
2023-10-24 18:44:23 +0100

    commit

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -4,13 +4,14 @@ import "../Styles/games.css";
 import { Container } from "./Navbar";
 import { Link } from "react-router-dom";
 import {AiOutlineTwitter, AiFillGithub, AiFillLinkedin} from "react-icons/ai";
-import btm_img from "./btm_img.jpg";
+import BottomBg from "./btm_img.jpg";
 
 function Action(){
     const [actionData,setactionData] = useState([])
     const {toggle,inputValue} = useContext(Container)
     const input = inputValue;
-    const backgroundImage = btm_img;
+    const [loading, setLoading] = useState(true);
+
 
     const Api = "https://api.rawg.io/api/games";
     const Apikey = "6c02bff12c9046f5ad4ed383d6af9c5f";
@@ -33,12 +34,22 @@ function Action(){
 
     useEffect(() => {
         ActiongamesApi()
-    },[input])
+    },[input]);
+
+    useEffect(() => {
+        setTimeout(() => {
+        setLoading(false);
+        }, 5000);
+    }, []);
 
 
     return (
         <Fragment>
-            <div className="games_container" id={!toggle? "secondaryBgColor" : "mainBgColor" }>
+             {loading ? ( // Render loading spinner if loading is true
+              <div className="loading-spinner"></div>
+            ) : (
+            <div id={!toggle? "secondaryBgColor" : "mainBgColor" }>
+                <div className="games_container">
                 {actionData.map((games) => (
                     <Link to={`/${games.id}`} key={games.id} style={{textDecoration:"none"}} >
                      <div className="container" id={!toggle? "secondaryColor" : "mainColor"}>
@@ -49,27 +60,30 @@ function Action(){
                 ))}
             </div>
 
-            <div className="bottom_bg" style={{
-              backgroundImage: `url(${backgroundImage})`,
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-              height:"300px",
-            }}>
-                <h2>Connect with me</h2>
-                <div className="iconContainer">
-                    <a href="https://twitter.com/9gunna9">
-                        <AiOutlineTwitter fontSize={30} color="#1DA1F2"/>
-                    </a>
-                     <a href="https://github.com/Dev-Emmyy">
-                        <AiFillGithub fontSize={30} color="000"/>
-                    </a>
-                     <a href="https://www.linkedin.com/in/ugochukwu-emmanuel-ba798a25a/">
-                        <AiFillLinkedin fontSize={30} color="#0A66C2"/>
-                    </a>
-                </div>
-                <h2>Built by Dev-Emmy</h2>
+            <div className="bottom_bg">
+            <img src={BottomBg} width="100%" height="350px" />
+            <div className="profile_link">
+              <div className="contact">
+                <h2>Connect Us</h2>
+              </div>
+              <div className="profile">
+                <a href="https://www.linkedin.com/in/ugochukwu-emmanuel-ba798a25a/">
+                  <AiFillLinkedin color="#0072b1" fontSize={30} cursor="pointer" fontWeight="bolder" />
+                </a>
+                <a href="https://twitter.com/9Gunna9">
+                  <AiOutlineTwitter color="#1DA1F2" fontSize={30} fontWeight="bolder" />
+                </a>
+                <a href="https://github.com/Dev-Emmyy">
+                  <AiFillGithub color="black" fontSize={30} cursor="pointer" fontWeight="bolder" />
+                </a>
+              </div>
+              <div>
+                <h2>My Challenge 1 project, Created by Dev-Emmy</h2>
+              </div>
             </div>
+          </div>
+          </div>
+            )}
         </Fragment>
     )
 }
