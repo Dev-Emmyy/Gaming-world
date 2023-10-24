d8ab4f9d9c59a867427575a0562c9afcbf3c9858
Gunna9999 <emmanuelugochukwu2000@gmail.com>
2023-10-24 19:01:11 +0100

    commit

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,18 +1,17 @@
 import { Fragment, useContext, useEffect, useState } from "react";
-import {AiFillLinkedin,AiOutlineTwitter,AiFillGithub} from "react-icons/ai"
 import axios from "axios";
 import { Container } from "./Navbar";
 import { useParams } from "react-router-dom";
 import logo from "./logo.png";
 import "../Styles/details.css";
 import {GoPrimitiveDot} from "react-icons/go";
-import BottomBg from "./btm_img.jpg";
 
 function Details(){
     const [detailsData,setDetailsData] = useState({});
     const {toggle} = useContext(Container);
     const backgroundStyle = `${detailsData.background_image}`
     const {id} = useParams();
+    const [loading, setLoading] = useState(true);
 
     const Api = `https://api.rawg.io/api/games/${id}`;
     const Apikey = "6c02bff12c9046f5ad4ed383d6af9c5f";
@@ -35,9 +34,17 @@ function Details(){
         DetailsApi()
     },[]);
 
+    useEffect(() => {
+    setTimeout(() => {
+      setLoading(false); 
+    }, 5000); 
+    }, [])
 
     return (
         <Fragment>
+            {loading ? (
+                <div className="loading-spinner"></div>
+            ) : (
             <div style={{backgroundImage : `url(${backgroundStyle})`,backgroundSize:'cover',backgroundRepeat: 'no-repeat',height: '1200px',backgroundPosition: 'center'}}>
             <div className="details_overlay"></div>
             <div className="details_container">
@@ -148,31 +155,8 @@ function Details(){
                 </div>
                </div>
          </div>
-
-          <div className="bottom_bg">
-                <img src={BottomBg} width="100%" height="350px" />
-                <div className="profile_link">
-                <div className="contact">
-                    <h2>Connect Us</h2>
-                </div>
-                <div className="profile">
-                    <a href="https://www.linkedin.com/in/ugochukwu-emmanuel-ba798a25a/">
-                    <AiFillLinkedin color="#0072b1" fontSize={30} cursor="pointer" fontWeight="bolder" />
-                    </a>
-                    <a href="https://twitter.com/9Gunna9">
-                    <AiOutlineTwitter color="#1DA1F2" fontSize={30} fontWeight="bolder" />
-                    </a>
-                    <a href="https://github.com/Dev-Emmyy">
-                    <AiFillGithub color="black" fontSize={30} cursor="pointer" fontWeight="bolder" />
-                    </a>
-                </div>
-
-                <div>
-                    <h2>My Challenge 1 project, Created by Dev-Emmy</h2>
-                </div>
-             </div>
-           </div>
         </div>
+         )}
         </Fragment>
     )
 }
